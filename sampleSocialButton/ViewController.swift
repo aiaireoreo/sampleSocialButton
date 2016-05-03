afed54f2e7cc8589cb9e8e7cf5980e3a60dfb168
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-03 15:55:49 +0800

    initial commit

--- sampleSocialButton/ViewController.swift
+++ sampleSocialButton/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
-
+    
+    //あとからFBボタンも作るので混同しないよう名前をつける
+    @IBOutlet weak var twitterBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapTwitterBtn(sender: UIButton) {
+        
+        //print("押されたよ")
+        //ボタンを作ったらまず押されたよと反応があるか確認
+        
+        var twitterVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        twitterVC.setInitialText("つぶやくよ")
+        presentViewController(twitterVC, animated: true, completion: nil)
+    }
+    
+    //TODO:FBのシェアボタンを作ろう
+    @IBAction func tapFacebookBtn(sender: UIButton) {
+        
+        var facebookVC =
+            SLComposeViewController(forServiceType:
+            SLServiceTypeFacebook)
+        facebookVC.setInitialText("フェイスブックだよ")
+        presentViewController(facebookVC, animated: true, completion: nil)
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
